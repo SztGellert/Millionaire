df3cd427c042f59215f2501d4191732abcd7c36b
SztGellert <sztankgellert@gmail.com>
2022-10-27 11:35:27 +0200

    --enh: added utils package to remove code duplications

--- millionaire/menu/menu.py
+++ millionaire/menu/menu.py
@@ -1,13 +1,9 @@
-import os
-import pathlib
 import sys
 import time
 import keyboard
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "1"
-import pygame
+from utils import utils
 from sty import Style, RgbFg, fg, bg
 
-operating_system = os.name
 options = ["Play", "Help", "Credit", "Exit"]
 fg.purple = Style(RgbFg(148, 0, 211))
 select_msg = "Select Menu Option: "
@@ -16,11 +12,10 @@ bg.orange = bg(255, 150, 50)
 
 
 def intro():
-    pygame.mixer.init()
-    clear_screen()
-    play_sound("loim_intro.wav", 0)
+    utils.clear_screen()
+    utils.play_sound("loim_intro.wav", 0)
     time.sleep(2)
-    file = (open_file("intro.txt", 'r'))
+    file = (utils.open_file("intro.txt", 'r'))
     for line_index in range(len(file)):
         if line_index == 3:
             print(fg.purple + file[line_index][0] + fg.rs)
@@ -32,7 +27,7 @@ def intro():
 
 def show_title():
     line_length = len(select_msg) + 3
-    clear_screen()
+    utils.clear_screen()
     print("=" * line_length)
     print(fg.purple + " ♦ WHO WANTS TO BE A ♦" + fg.rs)
     print("=" * line_length)
@@ -67,55 +62,20 @@ def exit():
     sys.exit(0)
 
 
-def clear_screen():
-    if operating_system == "posix":
-        os.system('clear')
-    else:
-        os.system('cls')
-
-
-def play_sound(filename, starting_time):
-    file_path = get_data_path() + "/sound_files/" + filename
-    pygame.mixer.init()
-    pygame.mixer.music.load(file_path)
-    pygame.mixer.music.set_volume(0.07)
-    pygame.mixer.music.play(0, starting_time)
-
-
-def get_data_path() -> str:
-    if operating_system == "posix":
-        path = str(pathlib.Path(__file__).parent.parent.parent.resolve())
-        data_path = path + "/data"
-    else:
-        data_path = "../data"
-
-    return data_path
-
-
 def help():
-    clear_screen()
-    file = (open_file("tutorial.txt", 'r'))
+    utils.clear_screen()
+    file = (utils.open_file("tutorial.txt", 'r'))
     for line in file:
         print(line[0])
     return_prompt()
 
 
 def credits():
-    clear_screen()
-    file = (open_file("credits.txt", 'r'))
+    utils.clear_screen()
+    file = (utils.open_file("credits.txt", 'r'))
     for line in file:
         print(line[0])
     return_prompt()
-
-
-def open_file(filename, mode):
-    file_path = get_data_path() + "/text_files/" + filename
-    with open(file_path, mode) as file:
-        list_of_file = []
-        for line in file:
-            line = line.strip().split(',')
-            list_of_file.append(line)
-    return list_of_file
 
 
 def return_prompt():
--- millionaire/runner.py
+++ millionaire/runner.py
@@ -22,6 +22,5 @@ def main():
             menu.exit()
 
 
-
 if __name__ == "__main__":
     main()
