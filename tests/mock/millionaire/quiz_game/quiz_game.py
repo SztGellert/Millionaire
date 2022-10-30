a0fdcf15cf833ab1c0ffa97bde21b755ce334765
SztGellert <sztankgellert@gmail.com>
2022-10-30 20:03:23 +0100

    --enh: runtime tests of guessing out of game and phone help

--- tests/mock/millionaire/quiz_game/quiz_game.py
+++ tests/mock/millionaire/quiz_game/quiz_game.py
@@ -17,9 +17,12 @@ prizes = ["5.000 Ft", "10.000 Ft", "25.000 Ft", "50.000 Ft", "100.000 Ft", "200.
 
 
 def play(inputs: dict):
+    out_of_game_inputs = inputs["out_of_game_answers"]
     game_inputs = inputs["game_answers"]
-    help_inputs = inputs["help_answers"]
-    help_types = {"audience": True, "telephone": True, "halving": True}
+    audience_inputs = inputs["audience_answers"]
+    halving_inputs = inputs["halving_answers"]
+    phone_inputs = inputs["phone_answers"]
+    help_types = {"audience": True, "halving": True, "phone": True}
     util.clear_screen()
     util.play_sound("lom.mp3", 0)
     time.sleep(2)
@@ -48,12 +51,12 @@ def play(inputs: dict):
         while answer not in list(answers.keys()):
             if answer == "t":
                 util.play_sound("music_off.mp3", 0)
-                if game_inputs[i] == "OK":
+                if out_of_game_inputs[0] == "OK":
                     answer = safe_input("\nSelect the correct answer ('a','b','c','d') ! ",
-                                        ["a", "b", "c", "d"], correct_answer_key)
+                        ["a", "b", "c", "d"], correct_answer_key)
                 else:
                     answer = safe_input("\nSelect the correct answer ('a','b','c','d') ! ",
-                                        ["a", "b", "c", "d"], game_inputs[i])
+                                        ["a", "b", "c", "d"], out_of_game_inputs[0])
                 time.sleep(2)
                 util.clear_screen()
                 util.play_sound("marked.mp3", 0)
@@ -76,38 +79,63 @@ def play(inputs: dict):
                     print(fg.red + "Bad answer! Better luck next time!" + fg.rs)
                     util.play_sound("so_sorry.mp3", 0)
                     time.sleep(1)
-                if safe_input("Would you like to play again? ('y'/'n')", ['y', 'n'], game_inputs[i]) == 'y':
+                if safe_input("Would you like to play again? ('y'/'n')", ['y', 'n'], out_of_game_inputs[1]) == 'y':
                     util.clear_screen()
                     play(inputs)
                 else:
                     sys.exit(0)
             if answer == "h":
-                help_functions = {"audience": audience_help, "telephone": telephone_help, "halving": halving}
-                chosen_help_type = safe_input("Choose help: 'a' for audience, 't' for telephone, 'h' for halving! ",
-                                              ["a", "t", "h"], help_inputs[0])
+                help_functions = {"audience": audience_help, "halving": halving, "telephone": telephone_help}
+                help_inputs = [audience_inputs, halving_inputs, phone_inputs]
+                chosen_help = str
+                for x in range(len(help_types)):
+                    if help_types[list(help_types)[x]] and (len(help_inputs[x]) > 0):
+                        chosen_help = list(help_types)[x]
+                chosen_input = ""
+                chosen_help_values = []
+                for help in help_inputs:
+                    if len(help) > 0:
+                        if chosen_help == "audience":
+                            chosen_help_values = help_inputs[0]
+                            chosen_input = "a"
+                        if chosen_help == "halving":
+                            chosen_help_values = help_inputs[1]
+                            chosen_input = "h"
+                        if chosen_help == "phone":
+                            chosen_help_values = help_inputs[2]
+                            chosen_input = "t"
+                chosen_help_type = safe_input("Choose help: 'a' for audience, 'h' for halving, 't' for telephone! ",
+                                              ["a", "h", "t"], chosen_input)
                 for x in range(len(help_types)):
                     if chosen_help_type.lower() == list(help_types)[x][0]:
                         if help_types[list(help_types)[x]]:
-                            list(help_functions.values())[x](question, shuffled_answers, correct_answer_value)
+                            if chosen_help != "phone":
+                                list(help_functions.values())[x](question, shuffled_answers, correct_answer_value)
+                            else:
+                                list(help_functions.values())[x](question, shuffled_answers, correct_answer_value, chosen_help_values[1])
                             help_types[list(help_types)[x]] = False
                             break
                         else:
                             print("You have already used the " + list(help_types)[x] + " help!")
-                            if help_inputs[1] == "OK":
-                                answer = safe_input("\nSelect the correct answer ('a','b','c','d'), 't' for guessing out of game or 'h' for help! ",
-                                                    ["a", "b", "c", "d", "h", "t"], correct_answer_key)
-                            else:
-                                answer = safe_input("\nSelect the correct answer ('a','b','c','d'), 't' for guessing out of game or 'h' for help! ",
-                                                    ["a", "b", "c", "d", "h", "t"], help_inputs[1])
-                if help_inputs and help_inputs[1] == "OK":
-                    answer = safe_input(
-                        "\nSelect the correct answer ('a','b','c','d'), 't' for guessing out of game or 'h' for help! ",
-                        ["a", "b", "c", "d", "h", "t"], correct_answer_key)
-                else:
-                    if help_inputs:
+                if chosen_help != "phone":
+                    if chosen_help_values[1] == "OK":
                         answer = safe_input(
                             "\nSelect the correct answer ('a','b','c','d'), 't' for guessing out of game or 'h' for help! ",
-                            ["a", "b", "c", "d", "h", "t"], help_inputs[1])
+                            ["a", "b", "c", "d", "h", "t"], correct_answer_key)
+                    else:
+                        answer = safe_input(
+                            "\nSelect the correct answer ('a','b','c','d'), 't' for guessing out of game or 'h' for help! ",
+                            ["a", "b", "c", "d", "h", "t"], chosen_help_values[1])
+                else:
+                    if chosen_help_values[2] == "OK":
+                        answer = safe_input(
+                            "\nSelect the correct answer ('a','b','c','d'), 't' for guessing out of game or 'h' for help! ",
+                            ["a", "b", "c", "d", "h", "t"], correct_answer_key)
+                    else:
+                        answer = safe_input(
+                            "\nSelect the correct answer ('a','b','c','d'), 't' for guessing out of game or 'h' for help! ",
+                            ["a", "b", "c", "d", "h", "t"], chosen_help_values[2])
+
                 time.sleep(2)
                 util.clear_screen()
         util.play_sound("marked.mp3", 0)
@@ -196,14 +224,14 @@ def print_phone_conversation(text: list, question: str, answers: {}, good_answer
     util.stop_sound()
 
 
-def telephone_help(question: str, answers: {}, correct_answer: str):
+def telephone_help(question: str, answers: {}, correct_answer: str, hotkey:str):
     phone = safe_input(
         "Who'd you like to call?\n"
         "for mum, press 'm'\n"
         "for dad press 'd'\n"
         "for old teacher from high school press 't'\n"
         "for Maester Yoda press 'y': ",
-        ["m", "d", "t", "y"])
+        ["m", "d", "t", "y"], hotkey)
     call_text_files = ["mum_phone.txt", "dad_phone.txt", "teacher_phone.txt", "yoda_master_phone.txt"]
     for i in range(len(call_text_files)):
         if phone.lower() == call_text_files[i][0]:
--- tests/mock_test.py
+++ tests/mock_test.py
@@ -6,39 +6,49 @@ from mock.millionaire import runner
 
 
 def main():
-    use_halving_help()
-    use_audience_help()
-    use_phone_help()
-    select_correct_answers()
+    test_select_correct_answers()
+    test_guess_out_of_the_game()
+    test_use_audience_help()
+    test_use_halving_help()
+    test_use_phone_help()
+    test_random_scenario()
 
 
-def select_correct_answers():
+def test_select_correct_answers():
     runner.main(["p", "e"], {
         "game_answers": ["OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK"],
-        "help_answers": []})
+        "audience_answers": [], "halving_answers": [], "phone_answers": [], "out_of_game_answers": []})
 
 
-def use_halving_help():
+def test_guess_out_of_the_game():
+    runner.main(["p", "e"], {
+        "game_answers": ["OK", "OK", "t", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK"],
+        "audience_answers": [], "halving_answers": [], "phone_answers": [], "out_of_game_answers": ["OK", "n"]})
+
+
+def test_use_halving_help():
+    runner.main(["p", "e"], {
+        "game_answers": ["h", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK"],
+        "audience_answers": [], "halving_answers": ["h", "OK"], "phone_answers": [], "out_of_game_answers": []})
+
+
+def test_use_audience_help():
     runner.main(["p", "e"], {
         "game_answers": ["OK", "h", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK"],
-        "help_answers": ["h", "OK"]})
+        "audience_answers": [], "halving_answers": ["h", "OK"], "phone_answers": [], "out_of_game_answers": []})
 
 
-def use_audience_help():
-    runner.main(["p", "e"], {
-        "game_answers": ["OK", "h", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK"],
-        "help_answers": ["h", "OK"]})
-
-
-def use_phone_help():
+def test_use_phone_help():
     runner.main(["p", "e"], {
         "game_answers": ["OK", "h", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK", "OK",
                          "OK"],
-        "help_answers": ["h", "m", "OK"]})
+        "audience_answers": [], "halving_answers": [], "phone_answers": ["t", "m", "OK"], "out_of_game_answers": []})
 
 
-def mock_test_scenario_one():
-    runner.main(["p", "e"], {"game_answers": ["a", "b", "c", "d"], "help_answers": []})
+def test_random_scenario():
+    runner.main(["p", "e"], {"game_answers": ["a", "b", "c", "d"],
+                             "audience_answers": [], "halving_answers": [], "phone_answers": [],
+                             "out_of_game_answers": []})
 
 
 if __name__ == "__main__":
