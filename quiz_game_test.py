3395e0e94af4eb21d69639a6f46c047ffa332fb6
SztGellert <sztankgellert@gmail.com>
2022-12-03 22:06:45 +0100

    --fix: tests

--- quiz_game_test.py
+++ quiz_game_test.py
@@ -1,19 +1,19 @@
 import unittest
 
-import millionaire.quiz_game as quiz
+from millionaire.quiz_game import quiz_game
 import json
 
 
 class TestSum(unittest.TestCase):
 
     def test_halving(self):
-        halved_answers = quiz.calculate_halved_answers({"a": "30", "b": "60", "c": "300", "d": "1"}, "60")
+        halved_answers = quiz_game.calculate_halved_answers({"a": "30", "b": "60", "c": "300", "d": "1"}, "60")
         self.assertIn("60", halved_answers.values())
         self.assertTrue(list(halved_answers.values()).count("") == 2)
 
     def test_audience_chances(self):
-        chances = quiz.get_chances({"a": "300", "b": "1", "c": "30", "d": "60"}, "60")
-        result = sum(chances)
+        chances = quiz_game.get_chances({"a": "300", "b": "1", "c": "30", "d": "60"}, "60")
+        result = sum(chances.values())
         self.assertEqual(100, result)
 
     """"
@@ -22,20 +22,6 @@ class TestSum(unittest.TestCase):
         pygame.init()
         chances = quiz_game.audience_help("How many seconds are in a minute?", {"a": "300", "b": "1", "c": "30", "d": "60"}, "60")
     """
-
-    def test_update_settings_file(self):
-        quiz.write_content_to_file("settings.json", {})
-        settings = [
-            {"lang": "en", "topic": "All"},
-            {"lang": "en", "topic": "Phisycs"},
-            {"lang": "hu", "topic": "Fizika"},
-            {"lang": "en", "topic": "Phisycs"}
-        ]
-        for i in settings:
-            with open("settings.json", "r", encoding="UTF-8") as file:
-                json.dump(settings, file)
-            file_data = json.load(file)
-            self.assertEqual(file_data, i)
 
 
 if __name__ == "__main__":
