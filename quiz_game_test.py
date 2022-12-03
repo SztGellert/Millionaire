43c773650fef92c6431969f1f05fd0ccdbfe2cf9
SztGellert <sztankgellert@gmail.com>
2022-12-03 21:54:08 +0100

    --fix: test package

--- quiz_game_test.py
+++ quiz_game_test.py
@@ -1,17 +1,16 @@
-from millionaire.quiz_game import quiz_game
-import unittest
+from ..mock import millionaire
 import json
 
 
 class TestSum(unittest.TestCase):
 
     def test_halving(self):
-        halved_answers = quiz_game.calculate_halved_answers({"a": "30", "b": "60", "c": "300", "d": "1"}, "60")
+        halved_answers = millionaire.quiz_game.calculate_halved_answers({"a": "30", "b": "60", "c": "300", "d": "1"}, "60")
         self.assertIn("60", halved_answers.values())
         self.assertTrue(list(halved_answers.values()).count("") == 2)
 
     def test_audience_chances(self):
-        chances = quiz_game.get_chances({"a": "300", "b": "1", "c": "30", "d": "60"}, "60")
+        chances = millionaire.quiz_game.get_chances({"a": "300", "b": "1", "c": "30", "d": "60"}, "60")
         result = sum(chances)
         self.assertEqual(100, result)
 
