2a23dacd4a2c593136ee3aebb460acdaa89d7cb8
SztGellert <sztankgellert@gmail.com>
2023-04-28 00:54:20 +0200

    use intro

--- runner.py
+++ runner.py
@@ -2,16 +2,12 @@
 
 from millionaire.menu import menu
 from millionaire.util import util
-from millionaire.quiz_game import quiz_game
 
 
 def main():
     util.init()
+    menu.intro()
     menu.main()
-
-    #quiz_game.play()
-    #menu.intro()
-    #menu.handle_main_menu()
 
 if __name__ == "__main__":
     main()
